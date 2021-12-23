b7120892e4b528f0c28f65bd82f0d211c27e7718
SamarpreetSingh <samar.guraya18@gmail.com>
2021-12-23 21:56:46 +0530

    feat: add react portal

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,26 +1,49 @@
 import React from "react";
 import styles from "./Error.module.css";
 import Button from "../UI/Button";
+import ReactDOM from "react-dom";
+
+const Backdrop = (props) => {
+  return (
+    <div
+      className={styles["backdrop"]}
+      onClick={() => props.setError(null)}
+    ></div>
+  );
+};
+
+const ErrorModal = (props) => {
+  return (
+    <div className={styles["error-modal"]}>
+      <div className={styles["error-head"]}>Invalid Input</div>
+      <div className={styles["error-msg"]}>
+        <p>{props.msg}</p>
+      </div>
+      <div className={styles["cancel-div"]}>
+        {/* <button onClick={clickHandler}>Okay</button> */}
+        <Button onClick={props.clickHandler}>Okay</Button>
+      </div>
+    </div>
+  );
+};
 
 const Error = (props) => {
   const clickHandler = (e) => {
     e.preventDefault();
-    props.setError("");
+    props.setError(null);
   };
 
   return (
-    <div className={styles["overlay"]}>
-      <div className={styles["error-div"]}>
-        <div className={styles["error-head"]}>Invalid Input</div>
-        <div className={styles["error-msg"]}>
-          <p>{props.msg}</p>
-        </div>
-        <div className={styles["cancel-div"]}>
-          {/* <button onClick={clickHandler}>Okay</button> */}
-          <Button onClick={clickHandler}>Okay</Button>
-        </div>
-      </div>
-    </div>
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop setError={props.setError} />,
+        document.getElementById("backdrop-div")
+      )}
+      {ReactDOM.createPortal(
+        <ErrorModal msg={props.msg} clickHandler={clickHandler} />,
+        document.getElementById("modal-div")
+      )}
+    </>
   );
 };
 
